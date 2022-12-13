7d8b21360c093b6a5265dbdf37f13419ae5caf15
dopewevmond <68214494+dopewevmond@users.noreply.github.com>
2022-12-13 14:02:54 +0000

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Basic from './components/MyApp';
+
+export const App = () => {
+  return (
+    <div className="p-2">
+      <Basic />
+    </div>
+  );
+};
---
+++ README.md
@@ -0,0 +1,3 @@
+# formik-ts
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-starter-typescript-wqnxew)
